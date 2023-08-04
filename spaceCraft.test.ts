73211c7b2c37fd6b27aa80626f34284d8e97dfa2
kulinrangani <kulinrangani01@gmail.com>
2023-08-04 12:07:58 +0530

    started monitizing to class base schema

--- spaceCraft.test.ts
+++ spaceCraft.test.ts
@@ -1,137 +1,18 @@
-import {
-  moveForward,
-  Position,
-  Command,
-  Direction,
-  rotateRight,
-  moveUpward,
-  moveBackward,
-  rotateLeft,
-  flipDirection
-} from "./spaceCraft";
-import spaceCraft from "./spaceCraft";
+import { SpaceCraft } from "./spaceCraft";
+import { Direction,Position,Command } from "./spaceCraft";
 
-describe("move forward", () => {
-  it("it shold move forward to the space", () => {
-    const currentPosition: Position = { x: 0, y: 0, z: 0 };
-    const cord = moveForward(currentPosition, "N");
-    expect(cord).toEqual({ x: 0, y: 1, z: 0 });
-  });
+describe("SpaceCraft", () => {
 
-  it("it shold move forward to the space", () => {
-    const currentPosition: Position = { x: 0, y: 1, z: 0 };
-    const cord = moveForward(currentPosition, "N");
-    expect(cord).toEqual({ x: 0, y: 2, z: 0 });
-  });
-});
-
-describe("rotate Right", () => {
-  it("it shold rotate right", () => {
-    const currentDirection: Direction = "N";
-    const actualOutput: Direction = rotateRight(currentDirection);
-    expect(actualOutput).toBe("E");
-  });
-});
-
-describe("rotate Left", () => {
-  it("it shold rotate left", () => {
-    const currentDirection: Direction = "U";
-    const actualOutput: Direction = rotateLeft(currentDirection);
-    expect(actualOutput).toBe("W");
-  });
-
-  it("it shold rotate left", () => {
-    const currentDirection: Direction = "E";
-    const actualOutput: Direction = rotateLeft(currentDirection);
-    expect(actualOutput).toBe("N");
-  });
-
-  it("it shold rotate north if prevDirection was east", () => {
-    const currentDirection: Direction = "U";
-    const actualOutput: Direction = rotateLeft(currentDirection, "E");
-    expect(actualOutput).toBe("W");
-  });
-});
-
-describe("gotoUpward", () => {
-  it("shold rotate to upward", () => {
-    const actualOutput: Direction = moveUpward();
-    expect(actualOutput).toBe("U");
-  });
-});
-
-describe("gotoBackward", () => {
-  it("it shold move to backward", () => {
-    const actualOutput: Position = moveBackward({ x: 0, y: 1, z: 0 }, "U");
-    expect(actualOutput).toEqual({ x: 0, y: 1, z: -1 });
-  });
-});
-
-describe("flipDirection",()=>{
-    it("should flip the direction",()=>{
-        const actualOutput :Direction= flipDirection("W");
-        expect(actualOutput).toBe("E");
+    let spaceCraft : SpaceCraft;
+    beforeAll(() => {
+        spaceCraft = new SpaceCraft();
     })
+    describe("move forward", () => {
+
+        it("it shold move forward to the space", () => {
+          const currentPosition: Position = { x: 0, y: 0, z: 0 };
+          const cord = spaceCraft.moveForward(currentPosition, "N");
+          expect(cord).toEqual({ x: 0, y: 1, z: 0 });
+        })
+  });
 })
-
-describe("spacecraft", () => {
-  it("Should move forward in the same direction  Command-1", () => {
-    const commands: Command[] = ["f"];
-    const initialDirection = "N";
-    const actualOutput: [Position, Direction] = spaceCraft(
-      commands,
-      initialDirection
-    );
-    expect(actualOutput).toEqual([{ x: 0, y: 1, z: 0 }, "N"]);
-  });
-
-  it("should move forward and then rotate to the right Command-2", () => {
-    const commands: Command[] = ["f", "r"];
-    const initialDirection = "N";
-    const actualOutput: [Position, Direction] = spaceCraft(
-      commands,
-      initialDirection
-    );
-    expect(actualOutput).toEqual([{ x: 0, y: 1, z: 0 }, "E"]);
-  });
-
-  it("Should move forward and then rotate to the rigth and move to upward Command-3", () => {
-    const commands: Command[] = ["f", "r", "u"];
-    const initialDirection = "N";
-    const actualOutput: [Position, Direction] = spaceCraft(
-      commands,
-      initialDirection
-    );
-    expect(actualOutput).toEqual([{ x: 0, y: 1, z: 0 }, "U"]);
-  });
-
-  it("Should move backward Command-4", () => {
-    const commands: Command[] = ["f", "r", "u", "b"];
-    const initialDirection = "N";
-    const actualOutput: [Position, Direction] = spaceCraft(
-      commands,
-      initialDirection
-    );
-    expect(actualOutput).toEqual([{ x: 0, y: 1, z: -1 }, "U"]);
-  });
-
-  it("Should move spaceCraft to left direction Command-5", () => {
-    const commands: Command[] = ["f", "r", "u", "b", "l"];
-    const initialDirection = "N";
-    const actualOutput: [Position, Direction] = spaceCraft(
-      commands,
-      initialDirection
-    );
-    expect(actualOutput).toEqual([{ x: 0, y: 1, z: -1 }, "W"]);
-  });
-
-  it("cutome cords", () => {
-    const commands: Command[] = ["b", "u", "b", "l", "f"];
-    const initialDirection = "E";
-    const actualOutput: [Position, Direction] = spaceCraft(
-      commands,
-      initialDirection
-    );
-    expect(actualOutput).toEqual([{ x: 0, y: 0, z: -1 }, "W"]);
-  });
-});
--- spaceCraft.ts
+++ spaceCraft.ts
@@ -1,134 +1,31 @@
 export type Direction = "N" | "E" | "S" | "W" | "U" | "B";
 export type Command = "f" | "r" | "u" | "b" | "l";
+
 export interface Position {
   x: number;
   y: number;
   z: number;
 }
-export const moveForward = (
-  currentPosition: Position,
-  currentDirection: Direction
-): Position => {
-  switch (currentDirection) {
-    case "N":
-      return { ...currentPosition, y: currentPosition.y + 1 };
-    case "E":
-      return { ...currentPosition, x: currentPosition.x + 1 };
-    case "S":
-      return { ...currentPosition, y: currentPosition.y + 1 };
-    case "W":
-      return { ...currentPosition, x: currentPosition.x + 1 };
-    default:
-      return currentPosition;
-  }
-};
+export class SpaceCraft {
+  currentPosition: Position;
+  currentDirection: Direction;
+  prevDirection: Direction;
 
-export const moveBackward = (
-  currentPosition: Position,
-  currentDirection: Direction
-): Position => {
-  switch (currentDirection) {
-    case "N":
-      return { ...currentPosition, y: currentPosition.y - 1 };
-    case "E":
-      return { ...currentPosition, x: currentPosition.x - 1 };
-    case "S":
-      return { ...currentPosition, y: currentPosition.y - 1 };
-    case "W":
-      return { ...currentPosition, x: currentPosition.x - 1 };
-    case "U":
-      return { ...currentPosition, z: currentPosition.z - 1 };
-    default:
-      return currentPosition;
-  }
-};
-
-export const rotateRight = (
-  currentDirection: Direction,
-  prevDirection?: Direction
-): Direction => {
-  switch (currentDirection) {
-    case "U":
-      return prevDirection || 'W'; 
-    case "N":
-      return "E";
-    case "E":
-      return "S";
-    case "S":
-      return "W";
-    case "W":
-      return "N";
-    default:
-      return currentDirection;
-  }
-};
-
-export const flipDirection = (direction : Direction) :Direction =>{
-    if (direction == "W") {
-        return "E";
-      } else if (direction == "S") {
-        return "N";
-      } else if (direction == "E") {
-        return "W";
-      } else if (direction == "N") {
-        return "S";
-      } else {
-        return "W";
-      } 
-}
-
-export const rotateLeft = (
-  currentDirection: Direction,
-  prevDirection?: Direction
-): Direction => {
-  switch (currentDirection) {
-    case "U":
-        return flipDirection(prevDirection? prevDirection : 'U');
-    case "N":
-      return "W";
-    case "W":
-      return "S";
-    case "S":
-      return "E";
-    case "E":
-      return "N";
-    default:
-      return currentDirection;
-  }
-};
-
-export const moveUpward = (): Direction => {
-  const directionWillBe: Direction = "U";
-  return directionWillBe;
-};
-
-export default function spaceCraft(
-  commands: Command[],
-  initialDirection: Direction
-): [Position, Direction] {
-  let currentPosition: Position = { x: 0, y: 0, z: 0 };
-  let currentDirection: Direction = initialDirection;
-  let prevDirection: Direction = initialDirection;
-  // console.log(commands);
-  for (let i = 0; i < commands.length; i++) {
-    switch (commands[i]) {
-      case "f":
-        currentPosition = moveForward(currentPosition, currentDirection);
+  moveForward(currentPosition : Position,currentDirection : Direction) :Position  {
+    switch (currentDirection) {
+      case "N":
+        currentPosition = { ...currentPosition, y: currentPosition.y + 1 };
         break;
-      case "r":
-        currentDirection = rotateRight(currentDirection, prevDirection);
+      case "E":
+        currentPosition = { ...currentPosition, x: currentPosition.x + 1 };
         break;
-      case "u":
-        prevDirection = currentDirection;
-        currentDirection = moveUpward();
+      case "S":
+        currentPosition = { ...currentPosition, y: currentPosition.y + 1 };
         break;
-      case "b":
-        currentPosition = moveBackward(currentPosition, currentDirection);
-        break;
-      case "l":
-        currentDirection = rotateLeft(currentDirection, prevDirection);
+      case "W":
+        currentPosition = { ...currentPosition, x: currentPosition.x + 1 };
         break;
     }
+    return currentPosition;
   }
-  return [currentPosition, currentDirection];
 }
