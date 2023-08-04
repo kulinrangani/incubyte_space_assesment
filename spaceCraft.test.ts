9ff361c4352cf70faa8a555e1377ede6271a5add
kulinrangani <kulinrangani01@gmail.com>
2023-08-04 12:44:40 +0530

    code formated

--- spaceCraft.test.ts
+++ spaceCraft.test.ts
@@ -14,10 +14,42 @@ describe("SpaceCraft", () => {
     });
   });
 
-  describe("rotate Right", () => {
-    it("it should rotate right", () => {
+  describe("rotateRight", () => {
+    it("should rotate right", () => {
       const currentDirection: Direction = "N";
       const actualOutput: Direction = spaceCraft.rotateRight(currentDirection);
+      expect(actualOutput).toBe("E");
+    });
+  });
+
+  describe("rotateLeft", () => {
+    it("should rotate left", () => {
+      const currentDirection: Direction = "N";
+      const actualOutput: Direction = spaceCraft.rotateLeft(currentDirection);
+      expect(actualOutput).toBe("W");
+    });
+  });
+
+  describe("moveUpward", () => {
+    it("shold rotate to upward", () => {
+      const actualOutput: Direction = spaceCraft.moveUpward();
+      expect(actualOutput).toBe("U");
+    });
+  });
+
+  describe("moveBackward", () => {
+    it("it shold move to backward", () => {
+      const actualOutput: Position = spaceCraft.moveBackward(
+        { x: 0, y: 1, z: 0 },
+        "U"
+      );
+      expect(actualOutput).toEqual({ x: 0, y: 1, z: -1 });
+    });
+  });
+
+  describe("flipDirection", () => {
+    it("should flip the direction", () => {
+      const actualOutput: Direction = spaceCraft.flipDirection("W");
       expect(actualOutput).toBe("E");
     });
   });
